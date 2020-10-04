e7cea384200669797e67b40125a59a1d9f0fca4b
Jose Alberto Garcia <joseegc10@gmail.com>
2020-10-04 13:45:19 +0200

    Añado comentarios a la clase partido, closes #12

--- src/partido.rb
+++ src/partido.rb
@@ -1,4 +1,12 @@
+# Clase que representa un partido de fútbol
+
 class Partido
+    # Un partido vendrá identificado por:
+    #      - Equipo local
+    #      - Equipo visitante
+    #      - Fecha del partido
+    #      - Goleadores
+
     def initialize(local, visitante, fecha)
         @local = local
         @visitante = visitante
@@ -6,10 +14,13 @@ class Partido
         @goleadores = Array.new
     end
 
+    # Métodos get de los atributos
     attr_reader :local
     attr_reader :visitante
     attr_reader :fecha
+    attr_reader :goleadores
 
+    # Método que añade un goleador al array de goleadores
     def aniadeGoleador(goleador)
         goleadores << goleador
     end
