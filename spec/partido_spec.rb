f89ac1613d09321f23427536b1d04e5a9d680e5c
Jose Alberto Garcia <joseegc10@gmail.com>
2020-10-15 16:35:30 +0200

    añado test de metodos de HU2 #2, tarea #23

--- spec/partido_spec.rb
+++ spec/partido_spec.rb
@@ -18,13 +18,39 @@ describe Partido do
         @nuevosGoleadores << @jugador2
     end
 
-	describe 'nombre' do
+    describe 'nombre' do
         it 'nombre del equipo local' do
             expect(@partido.local.nombre).to eq 'Real Madrid'
         end
 
         it 'nombre del equipo visitante' do
             expect(@partido.visitante.nombre).to eq 'Barsa'
+        end
+    end
+
+    describe '#aniadeGoleador' do
+        it 'nuevo goleador' do
+            @partido.aniadeGoleador(@jugador)
+            expect(@partido.goleadores.length).to eq(1)
+        end
+
+        it 'nombre goleador' do
+            @partido.aniadeGoleador(@jugador)
+            expect(@partido.goleadores[0].nombre).to eq 'Ramos'
+        end
+    end
+
+    describe '#aniadeGoleadores' do
+        it 'nuevos goleadores' do
+            @partido.aniadeGoleadores(@nuevosGoleadores)
+            expect(@partido.goleadores.length).to eq(2)
+        end
+
+        it 'nombres goleadores' do
+            @partido.aniadeGoleadores(@nuevosGoleadores)
+
+            expect(@partido.goleadores[0].nombre).to eq 'Ramos'
+            expect(@partido.goleadores[1].nombre).to eq 'Pique'
         end
     end
 end
