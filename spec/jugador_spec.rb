48c3066c47d252dc204be8c5974cdb194d21453e
Jose Alberto Garcia <joseegc10@gmail.com>
2020-10-15 17:54:24 +0200

    añado test de excepciones en clase jugador, tarea #37

--- spec/jugador_spec.rb
+++ spec/jugador_spec.rb
@@ -2,27 +2,33 @@ require_relative "../src/jugador.rb"
 require_relative "../src/equipo.rb"
 
 describe Jugador do 
-    equipo = Equipo.new('Real Madrid')
-    jugador = Jugador.new('Ramos', equipo)
+    before(:each) do
+        @equipo = Equipo.new('Real Madrid')
+        @jugador = Jugador.new('Ramos', @equipo)
+    end
     
     describe 'nombre' do
         it 'nombre del jugador' do
-            expect(jugador.nombre).to eq 'Ramos'
+            expect(@jugador.nombre).to eq 'Ramos'
         end
     end
 
     describe 'equipo' do
         it 'equipo del jugador' do
-            expect(jugador.equipo.nombre).to eq 'Real Madrid'
+            expect(@jugador.equipo.nombre).to eq 'Real Madrid'
         end
     end
 
     describe '#cambiaEquipo' do
         it 'jugador cambia de equipo' do
             nuevoEquipo = Equipo.new('Barsa')
-            jugador.cambiaEquipo(nuevoEquipo)
+            @jugador.cambiaEquipo(nuevoEquipo)
             
-            expect(jugador.equipo.nombre).to eq 'Barsa'
+            expect(@jugador.equipo.nombre).to eq 'Barsa'
+        end
+
+        it 'no es un equipo' do
+            expect{@jugador.cambiaEquipo(@jugador)}.to raise_error(ArgumentError)
         end
     end
 end
