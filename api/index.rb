4e02952da394a379aef5c4492c4286242c585163
Jose Alberto Garcia <joseegc10@gmail.com>
2020-11-20 19:21:30 +0100

    añado metodo para calcular el numero de dias hasta el proximo partido, avanzo HU3 #32, tarea #75, [skip ci]

--- api/index.rb
+++ api/index.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'json'
+require 'date'
+
 def cargaDatos()
     url = 'https://raw.githubusercontent.com/openfootball/football.json/master/2020-21/es.1.json'
     uri = URI(url)
@@ -5,6 +9,27 @@ def cargaDatos()
     partidos = JSON.parse(response)
   
     return partidos
+end
+
+def proximoPartido(partidos, equipo)
+    minimaDiferencia = Float::INFINITY
+  
+    for partido in partidos["matches"]
+        if (partido["team1"] == equipo or partido["team2"] == equipo) and not partido["score"]
+            nuevaFecha = Date.parse partido["date"]
+            diferencia = nuevaFecha - Date.today
+  
+            if diferencia < minimaDiferencia and diferencia >= 0
+                minimaDiferencia = diferencia
+            end
+        end
+    end
+  
+    if minimaDiferencia != Float::INFINITY
+        return Integer(minimaDiferencia)
+    else
+        return -1
+    end
 end
 
 Handler = Proc.new do |req, res|
