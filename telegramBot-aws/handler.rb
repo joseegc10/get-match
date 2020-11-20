05d66a4157e5757d06889362fa79245a7d321b0d
Jose Alberto Garcia <joseegc10@gmail.com>
2020-11-20 20:06:39 +0100

    bot de telegram de prueba, tarea #77, [skip ci]

---
+++ telegramBot-aws/handler.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'net/https'
+require 'date'
+
+def getMatch(event:, context:)
+    begin
+      data = JSON.parse(event["body"])
+      message = data["message"]["text"]
+      chat_id = data["message"]["chat"]["id"]
+      first_name = data["message"]["chat"]["first_name"]
+  
+      response = "Bot de prueba"
+  
+      payload = {text: response, chat_id: chat_id}
+  
+      uri = URI("https://api.telegram.org")
+  
+      Net::HTTP.start(uri.hostname, uri.port, {use_ssl: true}) do |http|
+        req = Net::HTTP::Post.new("/bot#{TOKEN}/sendMessage", {'Content-Type' => 'application/json; charset=utf-8'})
+        req.body = payload.to_json
+        http.request(req)
+      end
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+    end
+  
+    return { statusCode: 200 }
+  end
