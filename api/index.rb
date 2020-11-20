2c3389ef2b291f6548425646ca123d5c2a9c2dd5
Jose Alberto Garcia <joseegc10@gmail.com>
2020-11-20 19:16:46 +0100

    añado funcion para cargar datos desde url, tarea #75, [skip ci]

--- api/index.rb
+++ api/index.rb
@@ -1,3 +1,12 @@
+def cargaDatos()
+    url = 'https://raw.githubusercontent.com/openfootball/football.json/master/2020-21/es.1.json'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    partidos = JSON.parse(response)
+  
+    return partidos
+end
+
 Handler = Proc.new do |req, res|
     res.status = 200
     res['Content-Type'] = 'application/json; charset=utf-8'
