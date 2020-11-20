c96edcf9ec6b1dcd68b770268cb5ce088fa9a5af
Jose Alberto Garcia <joseegc10@gmail.com>
2020-11-20 19:25:02 +0100

    devuelvo dias para el proximo partido desde la funcion serverless, avanzo HU3 #32, tarea #75, [skip ci]

--- api/index.rb
+++ api/index.rb
@@ -36,7 +36,23 @@ Handler = Proc.new do |req, res|
     res.status = 200
     res['Content-Type'] = 'application/json; charset=utf-8'
   
-    msg = "Prueba"
+    equipo = req.query['equipo'] || 'Real Madrid'
+
+    partidos = cargaDatos()
+
+    dias = proximoPartido(partidos, equipo)
+
+    if dias != -1
+        if dias == 0
+            msg = "El próximo partido es hoy"
+        elsif dias == 1
+            msg = "Queda #{dias} día para el próximo partido del #{equipo}"
+        else
+            msg = "Quedan #{dias} días para el próximo partido del #{equipo}"
+        end
+    else
+        msg = "#{equipo} no es un equipo de la Liga Española"
+    end
   
-    res.body = {mensaje: msg}.to_json
+    res.body = {equipo: equipo, diasRestantes: dias, mensaje: msg}.to_json
 end
