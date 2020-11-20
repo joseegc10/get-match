778e3f41a18ffe2c7ea05a076e19d0d2c1b6ba3d
Jose Alberto Garcia <joseegc10@gmail.com>
2020-11-20 19:11:15 +0100

    prueba en vercel, tarea #75, [skip ci]

--- api/date.rb
+++
@@ -1,8 +0,0 @@
-Handler = Proc.new do |req, res|
-    res.status = 200
-    res['Content-Type'] = 'application/json; charset=utf-8'
-  
-    msg = "Prueba"
-  
-    res.body = {mensaje: msg}.to_json
-end
