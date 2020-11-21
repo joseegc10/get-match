4f6160f0ae005c096fc3371c2d686ef2c5330aaa
Jose Alberto Garcia <joseegc10@gmail.com>
2020-11-21 20:48:43 +0100

    test de cargar datos y proximo partido, avanza HU3 #32, tarea #75

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,1 +1,17 @@
 require_relative "../api/index.rb"
+
+describe 'carga datos vercel' do
+    it 'datos correctos vercel' do
+        partidos, equipos = cargaDatos()
+        expect(partidos["name"]).to eq 'Primera División 2020/21'
+        expect(equipos[0]).to eq 'SD Eibar'
+    end
+end
+
+describe '#proximoPartido' do
+    it 'proximo partido de un equipo' do
+        partidos, equipos = cargaDatos()
+        dias = proximoPartido(partidos, 'NoEstoyEnLiga')
+        expect(dias).to eq (-1)
+    end
+end
