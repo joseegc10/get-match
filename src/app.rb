e48fa27a425d5a773be4acf69e608c30be2fdb01
Jose Alberto Garcia <joseegc10@gmail.com>
2020-12-07 20:11:53 +0100

    añado ruta para calcular el resultado de un partido, avanza HU1 #1 y tarea #90

--- src/app.rb
+++ src/app.rb
@@ -12,4 +12,30 @@ class MyApp < Sinatra::Base
         content_type :json
         data_object.to_json
     end
+
+    # HU1: Como usuario, quiero poder consultar el resultado de un partido
+    get '/partido/resultado/:equipo/:jornada' do
+        numJornada = params['jornada'].to_i
+        nombreEquipo = params['equipo']
+
+        begin
+            resultado = @manejador.resultadoPartido(numJornada, nombreEquipo)
+
+            status 200
+            json(
+                {
+                    :Local => resultado.equipoLocal, 
+                    :Visitante => resultado.equipoVisitante, 
+                    :resultado => {
+                        :golesLocal => resultado.golesLocal,
+                        :golesVisitante => resultado.golesVisitante
+                    }
+                }
+            )
+        rescue => e
+            status 400
+            json({:status => e.message})
+        end
+        
+    end
 end
