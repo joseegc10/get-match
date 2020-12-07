94413755dcbf44f2236e2355f658e6a417034709
Jose Alberto Garcia <joseegc10@gmail.com>
2020-12-07 19:30:05 +0100

    reestructuro clase Partido para añadir atributo resultado, tarea #86

--- src/partido.rb
+++ src/partido.rb
@@ -2,6 +2,12 @@ require_relative "jugador.rb"
 require_relative "equipo.rb"
 require 'date'
 
+# Struct para el resultado del partido
+Resultado = Struct.new(:golesLocal, :golesVisitante, :equipoLocal, :equipoVisitante)
+
+# Struct para el par Goleador-número de goles
+Goleador_Goles = Struct.new(:goleador, :goles)
+
 # Clase que representa un partido de fútbol
 
 class Partido
@@ -10,11 +16,15 @@ class Partido
     #      - Equipo visitante
     #      - Fecha del partido
     #      - Goleadores
+    #      - Resultado del partido
 
     def initialize(local, visitante, fecha)
         @local = local
         @visitante = visitante
         @fecha = fecha
+
+        @resultado = resultado
+
         @goleadores = Array.new
     end
 
@@ -24,9 +34,45 @@ class Partido
     attr_reader :fecha
     attr_reader :goleadores   # HU2: consulta de goleadores
 
+    # Método que añade un goleador al array de goleadores a partir de su nombre y del nombre del equipo
+    def aniadeGoleadorNombre(nombreGoleador, nombreEquipo)
+        if nombreEquipo == @local.nombre
+            jugadores = @local.jugadores
+        elsif nombreEquipo == @visitante.nombre
+            jugadores = @visitante.jugadores
+        else
+            raise ArgumentError, 'El nombre del equipo no corresponde con un equipo del partido'
+        end
+
+        goleador = nil
+        for jugador in jugadores
+            if jugador.nombre == nombreGoleador
+                goleador = jugador
+            end
+        end
+
+        if goleador
+            @goleadores << goleador
+        else
+            raise ArgumentError, 'El nombre del goleador no corresponde con un jugador del partido'
+        end
+    end
+
     # Método que añade un goleador al array de goleadores
     def aniadeGoleador(goleador)
         raise ArgumentError, 'El parámetro no es un Jugador' unless goleador.is_a? Jugador
+
+        if (not @resultado)
+            @resultado = Resultado.new(0,0,@local,@visitante)
+        end
+
+        if (goleador.equipo.nombre == @local.nombre)
+            @resultado.golesLocal += 1
+        elsif (goleador.equipo.nombre == @visitante.nombre)
+            @resultado.golesVisitante += 1
+        else
+            raise ArgumentError, 'Hay al menos un goleador de un equipo que no juega el partido'
+        end
 
         @goleadores << goleador
     end
@@ -35,8 +81,21 @@ class Partido
     def aniadeGoleadores(nuevosGoleadores)
         raise ArgumentError, 'El parámetro no es un array de jugadores' unless nuevosGoleadores.is_a? Array
 
+        if (not @resultado)
+            @resultado = Resultado.new(0,0,@local,@visitante)
+        end
+
         for goleador in nuevosGoleadores
             raise ArgumentError, 'El parámetro no es un array de jugadores' unless goleador.is_a? Jugador
+    
+            if (goleador.equipo.nombre == @local.nombre)
+                @resultado.golesLocal += 1
+            elsif (goleador.equipo.nombre == @visitante.nombre)
+                @resultado.golesVisitante += 1
+            else
+                raise ArgumentError, 'Hay al menos un goleador de un equipo que no juega el partido'
+            end
+
             @goleadores << goleador
         end
     end
@@ -44,22 +103,11 @@ class Partido
     # Método que calcula el resultado de un partido teniendo en cuenta los goleadores
     # Se corresponde con la HU1: consulta del resultado de un partido
     def calculaResultado
-        golesLocal = 0
-        golesVisitante = 0
-
-        for goleador in @goleadores
-
-            if goleador.equipo.nombre == @local.nombre
-                golesLocal += 1
-
-            elsif goleador.equipo.nombre == @visitante.nombre
-                golesVisitante += 1
-                
-            end 
-
+        if @resultado
+            return @resultado
+        else
+            raise ArgumentError, 'El partido que piden no ha sido jugado'
         end
-
-        return [golesLocal, golesVisitante]
     end
 
     # Método que calcula el número de días que quedan para el partido
@@ -84,9 +132,13 @@ class Partido
 
     # Método que calcula el máximo goleador de un partido
     # Se corresponde con la HU4: consultar el máximo goleador de un partido
-    def maximoGoleador
-        maximoGoleador = ''
+    def maxGoleador
+        maximoGoleador = nil
         max_apariciones = 0
+
+        if not @resultado
+            raise ArgumentError, 'Ese equipo no ha jugado dicha jornada'
+        end
 
         for i in (0..(@goleadores.length)-1) do
             a_buscar = @goleadores[i].nombre
@@ -100,10 +152,11 @@ class Partido
 
             if num_apariciones > max_apariciones
                 max_apariciones = num_apariciones
-                maximoGoleador = a_buscar
+                maximoGoleador = @goleadores[i]
             end
         end
 
-        return [maximoGoleador, max_apariciones]
+        goleador_goles = Goleador_Goles.new(maximoGoleador, max_apariciones)
+        return goleador_goles
     end
 end
